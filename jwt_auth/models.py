bd50b2a2eb52ef4ed05fd187b8893ff79b737d6f
Jamie Maxwell <jdsmax19@gmail.com>
2020-04-19 21:05:35 +0100

    database has 2 courses

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -14,7 +14,6 @@ class User(AbstractUser):
   profileimage = models.ImageField(blank=True)
   video_of_swing = models.FileField(blank=True)
 
-
 class GolfBag(models.Model):
   putter = models.CharField(max_length=30)
   sw = models.CharField(max_length=30)
@@ -28,14 +27,23 @@ class GolfBag(models.Model):
 
   user = models.ForeignKey(User, related_name="usergolfbag", on_delete=models.PROTECT)
 
+  def __str__(self):
+    return f'{self.user}' 
+
 class UserGolfPhotos(models.Model):
 
   image = models.ImageField()
   user = models.ForeignKey(User, related_name="usergolfphotos", on_delete=models.PROTECT)
 
+  def __str__(self):
+    return f'{self.user} + {self.image}' 
+
 class UserHomeCourse(models.Model):
-   course = models.OneToOneField(Course, on_delete=models.PROTECT)
-   user = models.ForeignKey(User, related_name="userhomecourse", on_delete=models.PROTECT)
+  course = models.OneToOneField(Course, on_delete=models.PROTECT)
+  user = models.ForeignKey(User, related_name="userhomecourse", on_delete=models.PROTECT)
+
+  def __str__(self):
+    return f'{self.user} + {self.course}' 
 
 class UserCoursePlayed(models.Model):
 
@@ -46,18 +54,30 @@ class UserCoursePlayed(models.Model):
 
   user = models.ForeignKey(User, related_name="usercourseplayed", on_delete=models.CASCADE)
 
+  def __str__(self):
+    return f'{self.user} + {self.course}' 
+
 class UserCourseWishlist(models.Model):
 
   course = models.ManyToManyField(Course, related_name="coursewishlist", blank=True)
   user = models.ForeignKey(User, related_name="usercoursewishlist", on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.user} + {self.course}' 
 
 class UserCourseFavourites(models.Model):
 
   course = models.ManyToManyField(Course, related_name="coursefavourites", blank=True)
   user = models.ForeignKey(User, related_name="usercoursefavourites", on_delete=models.CASCADE)
 
+  def __str__(self):
+    return f'{self.user} + {self.course}' 
+
 class CourseComment(models.Model):
   comment = models.CharField(max_length=200)
   created_at = models.DateTimeField(auto_now_add=True)
   course = models.ForeignKey(Course, related_name="coursecomments", on_delete=models.PROTECT)
   user = models.ForeignKey(User, related_name='usercoursecomments', on_delete=models.PROTECT)
+
+  def __str__(self):
+    return f'{self.user} + {self.course}' 
